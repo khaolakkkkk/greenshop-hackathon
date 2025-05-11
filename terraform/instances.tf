a4b12c2e15c56dc6c70131572b63c01972193e31
nrageno <n.deruelle@ecole-ipssi.net>
2025-05-11 22:59:25 +0200

    Update instances.tf

--- terraform/instances.tf
+++ terraform/instances.tf
@@ -24,50 +24,54 @@ resource "aws_instance" "GS-INSTANCE-ADM" {
 }
 
 resource "aws_instance" "GS-INSTANCE-RPROXY" {
-  key_name                 = aws_key_pair.admin.key_name
-  ami                      = "ami-084568db4383264d4"
-  security_groups          = [aws_security_group.GS-SG-RPROXY.id]
-  subnet_id                = aws_subnet.GS-pub.id
-  instance_type            = "t2.micro"
+  key_name                    = aws_key_pair.admin.key_name
+  ami                         = "ami-084568db4383264d4"
+  security_groups             = [aws_security_group.GS-SG-RPROXY.id]
+  subnet_id                   = aws_subnet.GS-pub.id
+  instance_type               = "t2.micro"
   associate_public_ip_address = true
-  
-  user_data = <<-EOF
-              #!/bin/bash
-              apt update -y
-              apt install -y nginx
-              systemctl start nginx
-              systemctl enable nginx
-	      rm /etc/nginx/sites-enabled/default
 
+  user_data = <<EOF
+#!/bin/bash
+apt update -y
+apt install -y nginx
+systemctl start nginx
+systemctl enable nginx
+rm /etc/nginx/sites-enabled/default
 
-              cat > /etc/nginx/conf.d/load-balancer.conf <<EOL
-              upstream backend {
-                  server ${aws_instance.GS-INSTANCE-WEB1.private_ip};
-                  server ${aws_instance.GS-INSTANCE-WEB2.private_ip};
-                  server ${aws_instance.GS-INSTANCE-WEB3.private_ip};
-              }
+cat > /etc/nginx/conf.d/load-balancer.conf <<EOL
+upstream backend {
+    server ${aws_instance.GS-INSTANCE-WEB1.private_ip};
+    server ${aws_instance.GS-INSTANCE-WEB2.private_ip};
+    server ${aws_instance.GS-INSTANCE-WEB3.private_ip};
+}
 
-              server {
-                  listen 80;
+server {
+    listen 80;
 
-                  location / {
-                      proxy_pass http://backend;
-                      proxy_set_header Host \$host;
-                      proxy_set_header X-Real-IP \$remote_addr;
-                      proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-                      proxy_set_header X-Forwarded-Proto \$scheme;
-                  }
-              }
-              EOL
+    location / {
+        proxy_pass http://backend;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+    }
+}
+EOL
 
-              systemctl restart nginx
-              EOF
+systemctl restart nginx
+EOF
+
+  depends_on = [
+    aws_instance.GS-INSTANCE-WEB1,
+    aws_instance.GS-INSTANCE-WEB2,
+    aws_instance.GS-INSTANCE-WEB3
+  ]
 
   tags = {
     Name = "GS-INSTANCE-RPROXY"
   }
 }
-
 
 resource "aws_instance" "GS-INSTANCE-WEB1" {
   key_name        = aws_key_pair.admin.key_name
